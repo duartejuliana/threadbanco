118adc113dd559bf36a9022fe81e1ff8482b7502
Juliana Faustino de Freitas Duarte <cin_jduarte@uolinc.com>
2017-11-14 11:00:44 -0200

    adicionados integrantes

--- src/threadBanco/Transacao.java
+++ src/threadBanco/Transacao.java
@@ -1,5 +1,7 @@
 // Grupo:
 // Filipe Augusto RA 1421631
+// Honorio Martins RA1421554
+// Jefferson Inacio RA: 1423234
 // Juliana Duarte RA 1420539
 
 // O programa simula o funcionamento do sistema de transações de sacar, depositar, transferir e ver saldo em contas de um Banco.
